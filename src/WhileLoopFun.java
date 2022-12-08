49c4366a563b548d311633c10db7e56462ef4be5
gargs1 <gargs@wlhs.wlwv.k12.or.us>
2022-12-08 09:11:51 -0800

    Completed example.

--- src/WhileLoopFun.java
+++ src/WhileLoopFun.java
@@ -25,21 +25,44 @@ public class WhileLoopFun {
 
     public static void main(String[] args) {
         //count to 10
-
+        int counter = 1;
+        while(counter <= 10) {
+            System.out.print(counter + " ");
+            counter+= 2;
+        }
 
         //what does this code loop do? Do this without running it
         int count = 2;
         while(count <= 100){
-            System.out.print(count + " ");
             count *= 2;
+            System.out.print(count + " ");
+
         }
         System.out.println();
         //write a loop that counts by 5 until you get to 100
+        int counterTwo = 0;
+        while(counterTwo <= 100) {
+            System.out.println(counterTwo + " ");
+            counterTwo += 5;
+        }
 
         //write a loop that counts down from 10 to 1 then prints "Blast off!"
+        int counterThree = 10;
+        while(counterThree >=1) {
+            System.out.println(counterThree + " ");
+            counterThree --;
+        }
 
         //loop mistakes and while vs do while loop
         //make an infinite loop happen
+        int newCount = 5;
+        while(newCount >= 10) {
+            System.out.println("This statement has executed");
+        }
+        //outside of CSA scope
+        do{
+            System.out.println("This will print at least once.");
+        } while(newCount >= 10);
 
 
     }
